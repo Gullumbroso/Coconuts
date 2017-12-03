7959805a26f7efd921305bf121c9803a4a2313ad
Gilad Lumbroso <gullumbroso@gmail.com>
2017-12-03 19:53:41 +0200

    Initial commit

---
+++ Assets/Scripts/Monkey.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monkey : MonoBehaviour {
+
+	private float MAX_POS = 3.0f;
+	private float MIN_POS = -3.0f;
+	private float INIT_Y_POS = -1.3f;
+
+	private bool moveLeft;
+	private bool moveRight;
+	private float rotation;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		getInput ();
+	}
+
+	void getInput () {
+		float posX = transform.position.x;
+		Vector3 newPos = new Vector3(posX, transform.position.y, 0);
+
+		if (Input.GetKey (KeyCode.LeftArrow) && posX > MIN_POS) {
+			posX -= 0.05f;
+		}
+		if (Input.GetKey (KeyCode.RightArrow) && posX < MAX_POS) {
+			posX += 0.05f;
+		}
+			
+		newPos = new Vector3(posX, calcY(posX) + INIT_Y_POS, 0);
+		transform.position = newPos;
+		rotation = posX * 3.3f;
+		transform.rotation = Quaternion.Euler (0, 0, rotation);
+	}
+
+	float calcY(float x) {
+		return 0.1f * (float) System.Math.Pow (x, 2);
+	}
+
+	public float getRotation() {
+		return rotation;
+	}
+}
---
+++ Assets/Scripts/Turtle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour {
+
+	float MAX_ANGLE = 50.0f;
+	float MIN_ANGLE = -50.0f;
+	float MOVEMENT_OFFSET_DIV = -450f;
+	float MONKEY_ANGLE_OFFSET_DIV = 50f;
+	float MONKEY_HEIGHT = 0.9f;
+
+	private float rotation = 0f;
+	public GameObject monkey;
+	public Monkey monkeyScript;
+
+	// Use this for initialization
+	void Awake () {
+		monkey = GameObject.Find ("Monkey");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		getInput ();
+	}
+
+	void getInput() {
+
+		if (Input.GetKey (KeyCode.A) && rotation < MAX_ANGLE) {
+			rotation += 5f;
+		}
+		if (Input.GetKey (KeyCode.D) && rotation > MIN_ANGLE) {
+			rotation -= 5f;
+		}
+
+		float monkeyAngle = monkey.transform.rotation.eulerAngles.z;
+		monkeyAngle = (monkeyAngle > 180) ? monkeyAngle - 360 : monkeyAngle;
+		var offset = rotation / MOVEMENT_OFFSET_DIV;
+			
+		offset -= (monkeyAngle / MONKEY_ANGLE_OFFSET_DIV);
+
+		var monkeyPos = monkey.transform.position;
+		Vector3 newPos = new Vector3(monkeyPos.x + offset, monkeyPos.y + MONKEY_HEIGHT, monkeyPos.z);
+
+		transform.position = newPos;
+		float newRotation = rotation + monkeyAngle;
+		transform.rotation = Quaternion.Euler (0, 0, newRotation);
+	}
+}
